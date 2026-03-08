219e6a6c98e1f865f843d4ca02db368c81bc9e51
Philip\Philip <philipabdalla37>
2026-03-07 22:28:54 -0500

    update main with integration template

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import sys
 import serial
 import time
+import json
 import subprocess
 from gpiozero import Button
+sys.path.append('GM_RAG')
+sys.path.append('camera-vlm')
+from DM_RAG import DM_RAG
+
+from Die.src.DieDetection import DieDetection
 
 # 1. Import your local module
 sys.path.append('speech-to-text')
@@ -29,32 +35,97 @@ def speak_text(text):
     ps.wait()
 
 def main():
+
+    die = DieDetection()
+    filename = "character_sheet.json"
+
+    game_master = DM_RAG("test")
+    print("DB Loaded")
+
     # Setup Hardware
     ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=0.1)
     btn = Button(BUTTON_PIN, pull_up=False)
     
     # Setup Speech Engine
     engine = SpeechEngine(WHISPER_EXEC, WHISPER_MODEL)
-    
+
     print("--- Robot Ready ---")
 
+    start_game_message = """Hello Welcome to Dungeons and 
+    Dragons Game. I am your Dungeon Master. Everyone please
+    fill in your character sheet. Press Button once ready"""
+
+    speak_text(start_game_message)
+
+    print("Waiting for players to finish sheets... Press physical button to continue.")
+    btn.wait_for_press()  # This pauses the code until you physically push the button
+    speak_text("Thank you. Let's begin the character registration.")
+    
+    player_count = 0
+
     while True:
-        if btn.is_pressed:
-            # --- THE ONE FUNCTION CALL ---
-            # This blocks until the user stops talking and text is ready
-            message = engine.get_transcript(ser, btn)
+        speak_text(f"Player {player_count+1} place your sheet under the camera")
+    
+        # Logic to detect sheet goes here
+        # if not detect_sheet():
+        #     break 
+        
+        player_count += 1
+
+    print("Number of Players: ", player_count)
+
+    # TODO: Read charcter json file and make into a string (party_dsc)
+    try:
+        with open(filename, 'r') as f:
+            # Load the file as a Python dictionary
+            data = json.load(f)
             
-            print(f"User said: {message}")
+            # Convert it into a formatted string for the LLM
+            # indent=2 makes it readable for debugging; omit it for a shorter string
+            party_dsc = json.dumps(data, indent=2) 
             
-            speak_text(message)
-            
-            # TODO: Pass 'message' to Game Engine or Camera here
-            
-        else:
-            # Idle: Clear garbage data so the buffer is empty when you start talking
-            if ser.in_waiting > 0:
-                ser.read(ser.in_waiting)
-            time.sleep(0.01)
+        print("Party description prepared successfully.")
+    except FileNotFoundError:
+        print(f"Warning: {filename} not found. Using empty description.")
+        party_dsc = "{}"
+
+    game_round = 1
+    #llm_response, type_of_run = game_master.first_turn(party_dsc)
+
+    #speak_text(llm_response)
+
+    while True:
+        player_response = ""
+
+        if type_of_run == -1: #END OF GAME
+            speak_text(llm_response)
+
+            end_game_message = "GAME OVER"
+
+            speak_text(end_game_message)
+            break
+
+        if type_of_run == 0: #MIC Type of run
+            if btn.is_pressed:
+                # --- THE ONE FUNCTION CALL ---
+                # This blocks until the user stops talking and text is ready
+                player_response = engine.get_transcript(ser, btn)
+                
+                print(f"User said: {player_response}")
+                #DEBUGGING
+                #speak_text(player_response)            
+            else:
+                # Idle: Clear garbage data so the buffer is empty when you start talking
+                if ser.in_waiting > 0:
+                    ser.read(ser.in_waiting)
+                time.sleep(0.01)
+
+        if type_of_run == 1: #DIE Type of run
+            player_response = die.runDie()
+            print(f"Die number Detected: {player_response}")
+
+        game_round += 1
+        #llm_response, type_of_run = game_master.next_turn(llm_response, player_response, game_round)
 
 if __name__ == "__main__":
     main()
